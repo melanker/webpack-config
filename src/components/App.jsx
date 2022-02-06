7736fa698fe09fcb35e2cd2ede218cd6901a880b
melanker <alonm@traiana.com>
2022-02-06 11:26:07 +0200

    add react support

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import "../styles/index.scss"
+import Recipes from "./Recipes";
+
+const App = () => {
+    return (
+        <>
+         <section className="hero"/>
+         <main>
+             <h1>Oh Hai, React</h1>
+         </main>
+            <Recipes />
+        </>
+    );
+};
+
+export default App;
